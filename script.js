03ffb7290f433bd56968790422182835834af913
ngngng268 <thugiang268@gmail.com>
2026-02-04 22:12:04 +0700

    added a brief frame for the files

---
+++ script.js
@@ -0,0 +1,18 @@
+/*function add (ans, num) {
+if num === undefined => num=0}
+    
+function subtract
+function multiply
+function divide 
+
+function operate
+    if num = undefined operator = undefined then ans = ans 
+    else:
+        pick operator
+        ans = ?
+        num=undefined
+        return ans
+    
+function display to display what on screen: delete screen then print new value
+
+function reset: ans = 0; num=undefined ; operator = undefined*/
